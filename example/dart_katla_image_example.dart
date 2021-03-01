49b74ec15aec9e8c4dcd5dd17977a9dcedf50f5f
Simon Everts <ses@sanovogroup.com>
2021-03-01 14:54:14 +0100

    initial commit

---
+++ example/dart_katla_image_example.dart
@@ -0,0 +1,6 @@
+import 'package:dart_katla_image/dart_katla_image.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/dart_katla_image.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library dart_katla_image;
+
+export 'src/dart_katla_image_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
